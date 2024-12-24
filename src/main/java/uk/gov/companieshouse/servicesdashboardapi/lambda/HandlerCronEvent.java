0fbe92ffecd268b995d4926a9a72d87c503db480
stefano basile <sbasile@companieshouse.gov.uk>
2024-12-24 11:34:44 +0000

    set servicesController without @Autowired

--- src/main/java/uk/gov/companieshouse/servicesdashboardapi/lambda/HandlerCronEvent.java
+++ src/main/java/uk/gov/companieshouse/servicesdashboardapi/lambda/HandlerCronEvent.java
@@ -8,38 +8,39 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import uk.gov.companieshouse.servicesdashboardapi.controller.ServicesDashboardController;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class HandlerCronEvent implements RequestHandler<ScheduledEvent, Void> {
 
-    @Autowired
-    private ServicesDashboardController ServicesController;
+    private ServicesDashboardController servicesController;
+
+    public HandlerCronEvent(ServicesDashboardController servicesController) {
+        this.servicesController = servicesController;
+    }
 
     @Override
     public Void handleRequest(ScheduledEvent event, Context context) {
 
         LambdaLogger logger = context.getLogger();
         logger.log("triggering event received\n");
-        ServicesController.loadAllInfo();
 
-        // Map<String, Object> detail = event.getDetail();
+        Map<String, Object> detail = event.getDetail();
 
-        // if (detail.containsKey("action") && detail.get("action") instanceof String) {
+        if (detail.containsKey("action") && detail.get("action") instanceof String) {
 
-        //     String action = (String) detail.get("action");
-        //     logger.log("Action value: " + action + "\n");
+            String action = (String) detail.get("action");
+            logger.log("Action value: " + action + "\n");
 
-        //     if (action.equals("loadAllInfo")) {
-        //         ServicesController.loadAllInfo();
-        //         System.out.println("loadAllInfo");
-        //     }
+            if (action.equals("loadAllInfo")) {
+                servicesController.loadAllInfo();
+                System.out.println("loadAllInfo");
+            }
 
-        // } else {
-        //     logger.log("'action' field is missing or not a string\n");
-        // }
+        } else {
+            logger.log("'action' field is missing or not a string\n");
+        }
         return null;
     }
 }
