b12335e8fab2ce4400de342c7e3ce1e243a5751b
stefano basile <sbasile@companieshouse.gov.uk>
2024-12-24 10:18:16 +0000

    shade config

--- src/main/java/uk/gov/companieshouse/servicesdashboardapi/ServicesDashboardApiApplication.java
+++ src/main/java/uk/gov/companieshouse/servicesdashboardapi/ServicesDashboardApiApplication.java
@@ -3,39 +3,12 @@ package uk.gov.companieshouse.servicesdashboardapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Bean;
-
-import uk.gov.companieshouse.servicesdashboardapi.controller.ServicesDashboardController;
-import uk.gov.companieshouse.servicesdashboardapi.utils.ApiLogger;
-
-import java.util.function.Function;
-
-import uk.gov.companieshouse.servicesdashboardapi.lambda.CronEvent;
-
-
 @SpringBootApplication
 public class ServicesDashboardApiApplication {
 
   public static final String SDAPI_APP_NAMESPACE = "services-dashboard-api";
-  private final ServicesDashboardController servicesController;
-
-  public ServicesDashboardApiApplication(ServicesDashboardController servicesController) {
-    this.servicesController = servicesController;
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(ServicesDashboardApiApplication.class, args);
-  }
-
-  @Bean
-	public Function<CronEvent, Void> handleEvent() {
-    ApiLogger.info("triggering event received");
-    return event -> {
-      if (event.getAction().equals("loadAllInfo")) {
-        servicesController.loadAllInfo();
-        ApiLogger.info("loadAllInfo");
-      }
-      return null;
-    };
   }
 }
