cfce6756712c2c927fa4fe31359abbb7bf6fafea
stefano basile <sbasile@companieshouse.gov.uk>
2024-12-24 18:56:57 +0000

    test jar

---
+++ src/main/java/uk/gov/companieshouse/servicesdashboardapi/lambda/CronEvent.java
@@ -0,0 +1,14 @@
+package uk.gov.companieshouse.servicesdashboardapi.lambda;
+
+public class CronEvent {
+    private String action;
+
+    // Getters and setters
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
--- src/main/java/uk/gov/companieshouse/servicesdashboardapi/lambda/HandlerCronEvent.java
+++ src/main/java/uk/gov/companieshouse/servicesdashboardapi/lambda/HandlerCronEvent.java
@@ -1,46 +1,46 @@
-package uk.gov.companieshouse.servicesdashboardapi.lambda;
+// package uk.gov.companieshouse.servicesdashboardapi.lambda;
 
-import java.util.Map;
+// import java.util.Map;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
-import uk.gov.companieshouse.servicesdashboardapi.controller.ServicesDashboardController;
+// import com.amazonaws.services.lambda.runtime.Context;
+// import com.amazonaws.services.lambda.runtime.LambdaLogger;
+// import com.amazonaws.services.lambda.runtime.RequestHandler;
+// import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+// import uk.gov.companieshouse.servicesdashboardapi.controller.ServicesDashboardController;
 
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
 
-@Component
-public class HandlerCronEvent implements RequestHandler<ScheduledEvent, Void> {
+// @Component
+// public class HandlerCronEvent implements RequestHandler<ScheduledEvent, Void> {
 
-    private ServicesDashboardController servicesController;
+//     private ServicesDashboardController servicesController;
 
-    public HandlerCronEvent(ServicesDashboardController servicesController) {
-        this.servicesController = servicesController;
-    }
+//     public HandlerCronEvent(ServicesDashboardController servicesController) {
+//         this.servicesController = servicesController;
+//     }
 
-    @Override
-    public Void handleRequest(ScheduledEvent event, Context context) {
+//     @Override
+//     public Void handleRequest(ScheduledEvent event, Context context) {
 
-        LambdaLogger logger = context.getLogger();
-        logger.log("triggering event received\n");
+//         LambdaLogger logger = context.getLogger();
+//         logger.log("triggering event received\n");
 
-        Map<String, Object> detail = event.getDetail();
+//         Map<String, Object> detail = event.getDetail();
 
-        if (detail.containsKey("action") && detail.get("action") instanceof String) {
+//         if (detail.containsKey("action") && detail.get("action") instanceof String) {
 
-            String action = (String) detail.get("action");
-            logger.log("Action value: " + action + "\n");
+//             String action = (String) detail.get("action");
+//             logger.log("Action value: " + action + "\n");
 
-            if (action.equals("loadAllInfo")) {
-                servicesController.loadAllInfo();
-                System.out.println("loadAllInfo");
-            }
+//             if (action.equals("loadAllInfo")) {
+//                 servicesController.loadAllInfo();
+//                 System.out.println("loadAllInfo");
+//             }
 
-        } else {
-            logger.log("'action' field is missing or not a string\n");
-        }
-        return null;
-    }
-}
+//         } else {
+//             logger.log("'action' field is missing or not a string\n");
+//         }
+//         return null;
+//     }
+// }
