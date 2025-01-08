87490ed8f219dd2f5cf6870976a319849d94ffa2
stefano basile <sbasile@companieshouse.gov.uk>
2025-01-08 11:12:57 +0000

    add nslookup & dig test

--- src/main/java/uk/gov/companieshouse/servicesdashboardapi/ServicesDashboardApiApplication.java
+++ src/main/java/uk/gov/companieshouse/servicesdashboardapi/ServicesDashboardApiApplication.java
@@ -9,10 +9,12 @@ import uk.gov.companieshouse.servicesdashboardapi.utils.ApiLogger;
 
 import java.net.InetAddress;
 import java.util.function.Function;
-import java.net.UnknownHostException;
 
 import uk.gov.companieshouse.servicesdashboardapi.lambda.CronEvent;
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 // @EnableAutoConfiguration
 // @Import({
@@ -30,6 +32,7 @@ public class ServicesDashboardApiApplication {
     logHostInfo("code-analysis.platform.aws.chdev.org");
     logHostInfo("api.github.com");
     logHostInfo("endoflife.date");
+    testDNS("dependency-track.companieshouse.gov.uk");
   }
 
   private static void logHostInfo(String hostName) {
@@ -40,6 +43,35 @@ public class ServicesDashboardApiApplication {
       ApiLogger.info("Failed to resolve address: " + e.getMessage());
     }
   }
+
+
+
+  private  void testDNS(String hostName) {
+        String nslookupResult = runCommand("nslookup " + hostName);
+        String digResult = runCommand("dig " + hostName);
+        ApiLogger.info( String.format("NSLOOKUP Result:%s / DIG Result:%s", nslookupResult, digResult));
+    }
+
+    private String runCommand(String command) {
+        StringBuilder output = new StringBuilder();
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            process.waitFor();
+        } catch (Exception e) {
+            output.append("Error running command: ").append(e.getMessage()).append("\n");
+        }
+        return output.toString();
+    }
+
+
+
+
+
   public static void main(String[] args) {
     SpringApplication.run(ServicesDashboardApiApplication.class, args);
   }
