6d91ed15628b43ff233ee64d5bb26774b54bde06
stefano basile <sbasile@companieshouse.gov.uk>
2025-01-10 18:21:02 +0000

    tidy up

--- src/main/java/uk/gov/companieshouse/servicesdashboardapi/ServicesDashboardApiApplication.java
+++ src/main/java/uk/gov/companieshouse/servicesdashboardapi/ServicesDashboardApiApplication.java
@@ -13,9 +13,6 @@ import java.util.function.Function;
 import uk.gov.companieshouse.servicesdashboardapi.lambda.CronEvent;
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 // @EnableAutoConfiguration
 // @Import({
 //     ContextFunctionCatalogAutoConfiguration.class
@@ -32,7 +29,6 @@ public class ServicesDashboardApiApplication {
     logHostInfo("code-analysis.platform.aws.chdev.org");
     logHostInfo("api.github.com");
     logHostInfo("endoflife.date");
-    testDNS("dependency-track.companieshouse.gov.uk");
   }
 
   private static void logHostInfo(String hostName) {
@@ -43,34 +39,6 @@ public class ServicesDashboardApiApplication {
       ApiLogger.info("Failed to resolve address: " + e.getMessage());
     }
   }
-
-
-
-  private  void testDNS(String hostName) {
-        String nslookupResult = runCommand("nslookup " + hostName);
-        String digResult = runCommand("dig " + hostName);
-        ApiLogger.info( String.format("NSLOOKUP Result:%s / DIG Result:%s", nslookupResult, digResult));
-    }
-
-    private String runCommand(String command) {
-        StringBuilder output = new StringBuilder();
-        try {
-            Process process = Runtime.getRuntime().exec(command);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n");
-            }
-            process.waitFor();
-        } catch (Exception e) {
-            output.append("Error running command: ").append(e.getMessage()).append("\n");
-        }
-        return output.toString();
-    }
-
-
-
-
 
   public static void main(String[] args) {
     SpringApplication.run(ServicesDashboardApiApplication.class, args);
