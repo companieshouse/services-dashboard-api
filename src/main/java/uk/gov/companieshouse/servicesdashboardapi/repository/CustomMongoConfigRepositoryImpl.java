852e4cb389f3ed604227f63826d770ae123a01d8
stefano basile <sbasile@companieshouse.gov.uk>
2025-02-27 12:24:03 +0000

    tidy up Mongo config-collection set/update

--- src/main/java/uk/gov/companieshouse/servicesdashboardapi/repository/CustomMongoConfigRepositoryImpl.java
+++ src/main/java/uk/gov/companieshouse/servicesdashboardapi/repository/CustomMongoConfigRepositoryImpl.java
@@ -1,8 +1,6 @@
 package uk.gov.companieshouse.servicesdashboardapi.repository;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -35,14 +33,20 @@ public class CustomMongoConfigRepositoryImpl implements CustomMongoConfigReposit
    public void saveConfigInfo(MongoConfigInfo configInfo) {
       ApiLogger.info("saveConfigInfo START");
 
-      Query query = new Query(Criteria.where("id").is(singletonId));
+      Query query = new Query(Criteria.where("_id").is(singletonId));
       Update update = new Update()
-         .set("lastScan", LocalDateTime.ofInstant(
-            Instant.ofEpochMilli(System.currentTimeMillis()),
-            ZoneId.systemDefault())
-            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+         .set("lastScan", LocalDateTime.now()
+         .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
 
-      mongoTemplate.updateFirst(query, update, collectionName);
+      System.out.println("Query: " + query);
+      System.out.println("Update: " + update);
+
+      // Try first to update if the doc exists
+      if (mongoTemplate.updateFirst(query, update, collectionName).getModifiedCount() == 0) {
+         // If nothing was updated --> create it
+         ApiLogger.info("creating new config info");
+         mongoTemplate.upsert(query, update, collectionName);
+      }
       ApiLogger.info("saveConfigInfo END");
    }
 }
