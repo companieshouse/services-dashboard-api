d09aee0d022a8d93027413dc18cd4883e4389fec
stefano basile <sbasile@companieshouse.gov.uk>
2025-03-10 09:19:00 +0000

    update env vars

--- source.me.sh
+++ source.me.sh
@@ -9,9 +9,12 @@
 
  export DT_SERVER_APIKEY="${DT_SERVER_APIKEY}"
  export DT_SERVER_BASEURL='https://dependency-track.companieshouse.gov.uk'
- export SONAR_TOKEN="${DT_SONAR_TOKEN}"
  export GH_TOKEN="${GH_LAMBDA_TOKEN}"
 
  export AWS_PROFILE=dev
  export AWS_ENVS=cidev,staging
+ export SSM_PREFIX='/some/value'
 
+ export SONAR_URL='https://sonarqube.companieshouse.gov.uk'
+ #export SONAR_TOKEN="${DT_SONAR_TOKEN}"
+ export SONAR_TOKEN="${DT_SONAR_TOKEN_NEW}"
