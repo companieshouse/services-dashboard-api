26ede19f08c37b8bfe978fd47ef154f4e98cc48b
garethpowell-ch <gpowell1@companieshouse.gov.uk>
2026-09-21 13:15:23 +0100

    ASM-2825 Refactor: Add default constructor in `ConfigSecrets` for `SsmClient` instantiation and standardize formatting in `DepTrackGetDataService` methods

--- src/main/java/uk/gov/companieshouse/servicesdashboardapi/service/DepTrackGetDataService.java
+++ src/main/java/uk/gov/companieshouse/servicesdashboardapi/service/DepTrackGetDataService.java
@@ -32,25 +32,25 @@ public abstract class DepTrackGetDataService<T> {
 
     protected abstract T fetch();
 
-   public HttpEntity<String> setHeaders(List<Map.Entry<String, String>> keyValuePairs) {
-      HttpHeaders headers = new HttpHeaders();
-      for (Map.Entry<String, String> pair : keyValuePairs) {
-          headers.add(pair.getKey(), pair.getValue());
-      }
-      // Ensure the "headerApiKey" is always present (without duplication)
-      if (!headers.containsHeader(this.headerApiKey)) {
-          headers.add(this.headerApiKey, this.apiKey);
-      }
-      return new HttpEntity<>(headers);
-   }
+    public HttpEntity<String> setHeaders(List<Map.Entry<String, String>> keyValuePairs) {
+        HttpHeaders headers = new HttpHeaders();
+        for (Map.Entry<String, String> pair : keyValuePairs) {
+            headers.add(pair.getKey(), pair.getValue());
+        }
+        // Ensure the "headerApiKey" is always present (without duplication)
+        if (!headers.containsHeader(this.headerApiKey)) {
+            headers.add(this.headerApiKey, this.apiKey);
+        }
+        return new HttpEntity<>(headers);
+    }
 
-   public String setUri(List<Map.Entry<String, String>> queryParams) {
-      UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(this.baseUrl + this.endPoint);
+    public String setUri(List<Map.Entry<String, String>> queryParams) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(this.baseUrl + this.endPoint);
 
-      for (Map.Entry<String, String> param : queryParams) {
-          builder.queryParam(param.getKey(), param.getValue());
-      }
+        for (Map.Entry<String, String> param : queryParams) {
+            builder.queryParam(param.getKey(), param.getValue());
+        }
 
-      return builder.toUriString();
-  }
+        return builder.toUriString();
+    }
 }
